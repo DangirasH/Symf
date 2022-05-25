d97af20ff4daab09897f640e6e9893e951b20a3c
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-05-25 17:29:19 +0200

    all working

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/program', name:'program_')]
+class ProgramController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Series',
+         ]);
+    }
+}
