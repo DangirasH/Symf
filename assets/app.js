c399ddf421935d0a166459c5fc30eba272eba024
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-05-26 10:07:20 +0200

    bootstrap finished

--- assets/app.js
+++ assets/app.js
@@ -6,9 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
-
-console.log('Hello Webpack Encore !')
